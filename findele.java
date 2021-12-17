cd7f17f616ddd1855be16c6c8b6cadc020919679
Deepak kumar <deepak841999@gmail.com>
2021-12-17 23:24:09 +0530

    findele

---
+++ findele.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class findele{
+
+public static void main(String[] args) {
+    // write your code here
+    Scanner scn= new Scanner(System.in);
+    int n=scn.nextInt();
+    int[] arr= new int[n];
+    for(int i=0;i<arr.length;i++){
+        arr[i]=scn.nextInt();
+    }
+    int f=scn.nextInt();
+     int idx=-1;
+     for(int i=0;i<arr.length;i++){
+         if(arr[i]==f){
+             idx=i;
+         }
+     }
+     System.out.println(idx);
+ }
+
+}
