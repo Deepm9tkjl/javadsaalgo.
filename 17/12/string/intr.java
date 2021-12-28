1054676dedae1f75483e9f337c100cd7b48ba573
Deepak kumar <deepak841999@gmail.com>
2021-12-29 01:52:21 +0530

    sss

---
+++ 17/12/string/intr.java
@@ -0,0 +1,25 @@
+package string;
+
+import java.util.Scanner;
+
+public class intr {
+public static void main(String[] args){
+    Scanner scn= new Scanner(System.in);
+    String  s1="deepak";
+// String s2=scn.nextLine();
+    String s3=scn.nextLine();
+    // System.out.println(s2);
+    // System.out.println(s3);
+    // String s=scn.nextLine();
+    // System.out.println(s);
+    // System.out.println(s.length());
+// s.charAt(0)='z'; //not work
+// String s4="adcd";
+// for(int i=0;i<s4.length();i++){
+//     for(int j=i+1;j<=s4.length();j++ ){
+// System.out.println(s4.substring(i,j));
+// }}
+String s5=s1+ " " +s3;
+System.out.println(10+1+"ugyg" +89+90);
+}    
+}
